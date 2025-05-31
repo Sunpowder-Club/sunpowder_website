6841c9f76e8f716268d387922b449a0764e17955
Arderku <arderku@gmail.com>
2025-05-31 16:36:47 +0800

    Update next.config.ts

--- sunpowderweb/next.config.ts
+++ sunpowderweb/next.config.ts
@@ -2,8 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',
-  basePath: '/sunpowder_website',
-  assetPrefix: '/sunpowder_website',
+  // basePath: '/sunpowder_website',
+  // assetPrefix: '/sunpowder_website',
   trailingSlash: true,
   images: {
     unoptimized: true
