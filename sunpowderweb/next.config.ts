95680cefb9bbe701667486c689064571d12bbe1b
Arderku <arderku@gmail.com>
2025-05-31 16:30:33 +0800

    update build system

--- sunpowderweb/next.config.ts
+++ sunpowderweb/next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: 'export',
   basePath: '/sunpowder_website',
+  assetPrefix: '/sunpowder_website',
   trailingSlash: true,
   images: {
     unoptimized: true
